0812cc8145e2687b5366758bb1e262813600bae8
Rafchik-Disptick <rafaelkras123@gmail.com>
2018-10-21 18:49:40 +0300

    Classes_project v0.1

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Files bbb = new Files();
+            
+            string s = bbb.read();
+            listBox1.Items.Clear();
+            listBox1.Items.Add(s);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Files bbb = new Files();
+            bbb.Write(textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Files bbb = new Files();
+            MessageBox.Show("Count of repeated number of numbers: " + bbb.Transform().ToString());
+        }
+    }
+}
---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Text;
+
+namespace WindowsFormsApp1
+{
+
+
+    public class Files
+    {
+        public string read()
+        {
+            FileStream fstream = File.OpenRead("1.txt");
+            byte[] array = new byte[fstream.Length];
+            fstream.Read(array, 0, array.Length);
+            fstream.Close();
+            string text = Encoding.Default.GetString(array);
+            return text;
+        }
+
+        
+            public void Write(string _s)
+            {
+                FileStream fs = new FileStream("1.txt", FileMode.Create);
+                StreamWriter writer = new StreamWriter(fs);
+                StringBuilder output = new StringBuilder();
+                output.Append(_s);
+                writer.WriteLine(output.ToString());
+                writer.Close();
+            }
+        
+       
+
+        public int Transform()
+        {
+            string s = read();
+            int flag = 1;
+         int final = 0;
+            
+            for (int i = 0;i < s.Length - 2; i ++ )
+            {
+               if(s[i] != s[i+1])
+                {
+                    
+                    final += flag;
+                }
+               
+            }
+            return final;
+        }
+        
+    }
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+        ;
+          
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
